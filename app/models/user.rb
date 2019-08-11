a591b60e2d3b69ec94f797ab818c5290ca00efe7
Steven Senkowski <scsenkowski@gmail.com>
2019-08-11 14:59:13 -0400

    add create method to user with sessions for :user_id followed by a redirect to the home page.  Using strong params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
-    
+
+     def create
+      user = User.create(user_params)
+      session[:user_id] = user.id
+      redirect_to root_path
+    end
+   
+    private
+      def user_params
+        params.require(:user).permit(:username)
+      end
 end
